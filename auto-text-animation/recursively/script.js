efe69c972a9bc2801eadcc33a639781be45e7764
skipperr254 <skipperr254@gmail.com>
2024-08-07 11:32:43 +0300

    Added correct article for words starting with vowels

--- auto-text-animation/recursively/script.js
+++ auto-text-animation/recursively/script.js
@@ -4,19 +4,31 @@ const careers = ["YouTuber", "Software Developer", "Instructor", "Web Developer"
 let careerIndex = 0;
 let characterIndex = 0;
 
+// Next Task: Add dynamic adding of correct articles for words that start with vowels.
 
 function printText() {
-    container.innerHTML = `<h1>I am ${article} ${careers[careerIndex].slice(0, characterIndex)}</h1>`;
+
+    container.innerHTML = `<h1>I am ${careers[careerIndex] == "Instructor" ? "an" : "a"} ${careers[careerIndex].slice(0, characterIndex)}</h1>`;
+    // let article = "a";
     if (characterIndex >= careers[careerIndex].length) {
-        careerIndex++;
         characterIndex = 0;
-        console.log(careerIndex)
+        // article = careers[careerIndex][characterIndex] === "A" || "E" || "I" || "O" || "U" ? "an" : "a";
+        // console.log(article)
+        careerIndex++;
     }
+
+
+    // if (characterIndex === 0) {
+    //     console.log(careers[careerIndex][characterIndex])
+    // }
+
 
     if (careerIndex >= careers.length) {
         careerIndex = 0;
         characterIndex = 0;
     }
+
+
     characterIndex++;
     setTimeout(printText, 400);
 }
